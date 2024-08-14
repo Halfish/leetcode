51cf9aea64924e3fd1d6003513dbd553331d1d31
brucezhang <1597665055@qq.com>
2024-08-14 10:15:18 +0800

    optimize No.13

---
+++ solution/13_罗马数字/roman_to_integer_map.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+
+unordered_map<char, int> m = {
+  {'I', 1},
+  {'V', 5},
+  {'X', 10},
+  {'L', 50},
+  {'C', 100},
+  {'D', 500},
+  {'M', 1000},
+};
+
+int romanToInt(string s) {
+  int ans = 0;
+  for (int i = 0; i < s.length(); ++ i) {
+    if (i + 1 < s.length() && m[s[i + 1]] > m[s[i]]) {
+      ans -= m[s[i]];
+    } else {
+      ans += m[s[i]];
+    }
+  }
+  return ans;
+}
+
+
+int main() {
+  string s = "LVIII";
+  //string s = "MCMXCIV";
+  cout << romanToInt(s) << endl;
+  return 0;
+}
