2b5fb946c8c723cf6a9d325fac298ed0a1c8e7cb
brucezhang <1597665055@qq.com>
2024-08-14 10:41:55 +0800

    format No.13 & No.14

--- solution/13_罗马数字/roman_to_integer.cpp
+++ solution/13_罗马数字/roman_to_integer.cpp
@@ -3,60 +3,58 @@
 #include <vector>
 using namespace std;
 
-
 int romanToInt(string s) {
-  int ans = 0;
-  for (int i = 0; i < s.length(); ++ i) {
-    if (s[i] == 'I') {
-      if ((i + 1) < s.length() && (s[i + 1] == 'V' || s[i + 1] == 'X')) {
-        if (s[i + 1] == 'V') {
-          ans += 4;
-        } else if (s[i + 1] == 'X') {
-          ans += 9;
+    int ans = 0;
+    for (int i = 0; i < s.length(); ++i) {
+        if (s[i] == 'I') {
+            if ((i + 1) < s.length() && (s[i + 1] == 'V' || s[i + 1] == 'X')) {
+                if (s[i + 1] == 'V') {
+                    ans += 4;
+                } else if (s[i + 1] == 'X') {
+                    ans += 9;
+                }
+                i++;
+                continue;
+            }
+            ans += 1;
+        } else if (s[i] == 'X') {
+            if ((i + 1) < s.length() && (s[i + 1] == 'L' || s[i + 1] == 'C')) {
+                if (s[i + 1] == 'L') {
+                    ans += 40;
+                } else if (s[i + 1] == 'C') {
+                    ans += 90;
+                }
+                i++;
+                continue;
+            }
+            ans += 10;
+        } else if (s[i] == 'C') {
+            if ((i + 1) < s.length() && (s[i + 1] == 'D' || s[i + 1] == 'M')) {
+                if (s[i + 1] == 'D') {
+                    ans += 400;
+                } else if (s[i + 1] == 'M') {
+                    ans += 900;
+                }
+                i++;
+                continue;
+            }
+            ans += 100;
+        } else if (s[i] == 'V') {
+            ans += 5;
+        } else if (s[i] == 'L') {
+            ans += 50;
+        } else if (s[i] == 'D') {
+            ans += 500;
+        } else if (s[i] == 'M') {
+            ans += 1000;
         }
-        i ++;
-        continue;
-      }
-      ans += 1;
-    } else if (s[i] == 'X') {
-      if ((i + 1) < s.length() && (s[i + 1] == 'L' || s[i + 1] == 'C')) {
-        if (s[i + 1] == 'L') {
-          ans += 40;
-        } else if (s[i + 1] == 'C') {
-          ans += 90;
-        }
-        i ++;
-        continue;
-      }
-      ans += 10;
-    } else if (s[i] == 'C') {
-      if ((i + 1) < s.length() && (s[i + 1] == 'D' || s[i + 1] == 'M')) {
-        if (s[i + 1] == 'D') {
-          ans += 400;
-        } else if (s[i + 1] == 'M') {
-          ans += 900;
-        }
-        i ++;
-        continue;
-      }
-      ans += 100;
-    } else if (s[i] == 'V') {
-      ans += 5;
-    } else if (s[i] == 'L') {
-      ans += 50;
-    } else if (s[i] == 'D') {
-      ans += 500;
-    } else if (s[i] == 'M') {
-      ans += 1000;
     }
-  }
-  return ans;
+    return ans;
 }
 
-
 int main() {
-  //string s = "LVIII";
-  string s = "MCMXCIV";
-  cout << romanToInt(s) << endl;
-  return 0;
+    // string s = "LVIII";
+    string s = "MCMXCIV";
+    cout << romanToInt(s) << endl;
+    return 0;
 }
--- solution/14_最长公共子串/longest_common_sub_string.cpp
+++ solution/14_最长公共子串/longest_common_sub_string.cpp
@@ -3,41 +3,38 @@
 #include <vector>
 using namespace std;
 
-
-string longestCommonPrefix(vector<string>& strs) {
-  // ["flower", "flow", "flight"]
-  if (strs.empty()) {
-    return "";
-  }
-  if (strs.size() == 1) {
-    return strs[0];
-  }
-  int len = 0;
-  bool shouldBreak = false;
-  while (true) {
-    for (int i = 1; i < strs.size(); ++ i) {
-      if (strs[i].length() <= len || strs[0].length() <= len) {
-        shouldBreak = true;
-      }
-      if (strs[i][len] != strs[0][len]) {
-        shouldBreak = true;
-      }
-      if (shouldBreak) {
-        return strs[0].substr(0, len);
-      }
+string longestCommonPrefix(vector<string> &strs) {
+    // ["flower", "flow", "flight"]
+    if (strs.empty()) {
+        return "";
     }
-    len ++;
-  }
+    if (strs.size() == 1) {
+        return strs[0];
+    }
+    int len = 0;
+    bool shouldBreak = false;
+    while (true) {
+        for (int i = 1; i < strs.size(); ++i) {
+            if (strs[i].length() <= len || strs[0].length() <= len) {
+                shouldBreak = true;
+            }
+            if (strs[i][len] != strs[0][len]) {
+                shouldBreak = true;
+            }
+            if (shouldBreak) {
+                return strs[0].substr(0, len);
+            }
+        }
+        len++;
+    }
 }
-
 
 int main() {
-  vector<string> v;
-  v.push_back("flow");
-  v.push_back("fec");
-  //v.push_back("flower");
-  //v.push_back("flight");
-  cout << longestCommonPrefix(v) << endl;
-  return 0;
+    vector<string> v;
+    v.push_back("flow");
+    v.push_back("fec");
+    // v.push_back("flower");
+    // v.push_back("flight");
+    cout << longestCommonPrefix(v) << endl;
+    return 0;
 }
-
