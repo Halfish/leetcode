2a105e80ec2df13a13a77fcf32cf12fa38ec908e
brucezhang <1597665055@qq.com>
2024-11-01 09:53:31 +0800

    add No.24

--- solution/24_两两交换链表中的结点/swap_linked_list.cpp
+++ solution/24_两两交换链表中的结点/swap_linked_list.cpp
@@ -12,34 +12,31 @@ ListNode *swapPairs(ListNode *head) {
     if (!head) {
         return head;
     }
-    ListNode *newHead = nullptr;
+    ListNode *preHead = new ListNode(-1);
+    preHead->next = head;
 
-    ListNode *last = nullptr;
+    ListNode *last = preHead;   // 上一个链表对的结尾
     ListNode *prev = head;
     ListNode *curr = nullptr;
 
-    while (prev) {
+    while (prev && prev->next) {
         curr = prev->next;
-        if (!curr) {
-            break;
-        }
         ListNode *tmp = curr->next;
+
+        // 重新连接； last->prev->curr->tmp 改成 last->curr->prev->tmp
         curr->next = prev;
         prev->next = tmp;
-        if (!last) {
-            newHead = curr;
-        } else {
-            last->next = curr;
-        }
+        last->next = curr;
+
         last = prev;
         prev = tmp;
     }
-    return newHead ? newHead : head;
+    return preHead->next;
 }
 
 int main() {
     vector<int> v = {1, 2, 3, 4};
-    v = {1, 3, 2};
+    // v = {1, 3, 2};
     ListNode *head = new ListNode(v[0]);
     ListNode *curr = head;
     for (int i = 1; i < v.size(); ++i) {
